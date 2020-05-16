cb7e803ef2d1321511d08f0545d3d150e2912a47
griecohernan <griecohernan@gmail.com>
2020-05-16 12:08:16 -0300

    confirmacion de envio

---
+++ confirmacionenvio.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hernán Grieco</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome-free-5.13.0-web/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome-free-5.13.0-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</head>
+
+
+<body class="fondo-index">
+
+
+<?php include_once("menu.php");?>
+
+    <section class="altura-index">
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-12">
+                    <h1 class="pt-sm-5">Gracias por contactarte,</h1>
+                    <div class="row">
+                        <div class="col-12 my-3">
+                            <p>Pronto me pondré en contacto contigo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 d-sm-none text-right mb-5 depc-mobile-index">
+                    <p>Patrocinado por <br><a href="https://depcsuite.com" target="_blank"><span>DePc Suite</span></a>
+                    </p>
+                </div>
+    </section>
+
+
+    <?php include_once("footer.php");?>
+    
+   
+
+    <footer id="responsive">
+        <div class="container">
+            <div class="row">
+                <div class="col text-left">
+                    <a href="https://api.whatsapp.com/send?phone=541164187516&amp;text=Hola
+                     " target="_blank"><i class="fab fa-whatsapp"></i></a>
+                </div>
+                 <div class="col text-center">
+                            <a href="https://www.linkedin.com/in/hernangrieco/" target="_blank"> <i
+                            class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="col text-right" >
+                    <a href="https://github.com/griecohernan" target="_blank"><i class="fab fa-github"></i> </a>
+                 </div>
+                
+            </div>
+        </div>
+
+    </footer>
+
+
+
+
+
+</body>
+
+</html>
